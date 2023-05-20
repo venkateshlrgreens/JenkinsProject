1fbaf4ec60f3f217deee4e0dd38fc490bcf13b8e
venkateshlrgreens <venkateshlrgreensomr@gmail.com>
2023-05-20 11:29:53 +0530

    First Commit

---
+++ src/test/java/org/test/JavaRunner.java
@@ -0,0 +1,13 @@
+package org.test;
+
+import org.junit.Test;
+
+public class JavaRunner {
+	
+	@Test
+	public void tc() {
+		System.out.println(4/0);
+		System.out.println("I am successfull...!!");
+	}
+
+}
